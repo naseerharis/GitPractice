af2b0fd89e9129653c356c44d56ff5f3876f0777
naseerharis <naseer.haris@tekschool.us>
2022-06-22 09:52:02 -0700

    change in class

--- src/day/one/PracticClass.java
+++ src/day/one/PracticClass.java
@@ -3,7 +3,8 @@ package day.one;
 public class PracticClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("this is first line of code");
 
 	}
 
